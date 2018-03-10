c3e879d50a701b1eda9229a8b949005241b02710
Austin McCulloch <austinjames.mcculloch@uoit.net>
2018-03-10 10:59:12 -0500

    need to read file commit

--- question1.c
+++ question1.c
@@ -20,11 +20,20 @@ typedef struct{
 }queue;
 
 void push(proc process){
-    
+    queue *n = malloc(sizeof(queue));
+    n->process.name[256] = process.name[256];
+    n->process.priority = process.priority;
+    n->process.pid = process.pid;
+    n->process.runtime = process.runtime;
+
+    free(n);
 }
 
-void iterate(queue q){
-
+void iterate(queue *q){
+    queue * current = q;
+    while(current!=NULL){
+        printf("%d/n", current->process);
+    }
 }
 int main(void){
 
